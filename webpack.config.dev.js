7499a1d25c7f55f181cb7b44b5a7e6c2cfc16e1f
innazis <ziskind.inna@gmail.com>
2017-09-25 16:10:13 +0300

    deleted public/js folder from gitignore

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -7,8 +7,8 @@ module.exports = webpackMerge(commonConfig, {
 
     output: {
         path:path.resolve(__dirname, 'public/js/app'),
-        filename: 'bundle.js',
         publicPath: '/js/app/',
+        filename: 'bundle.js',
         chunkFilename: '[id].chunk.js'
     },
     module: {
